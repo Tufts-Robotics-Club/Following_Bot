c6a9f90924e1d267f3831b385655173a670b7313
Freja Perin <freja.perin@gmail.com>
2018-11-09 18:17:15 -0500

    finished test

--- Testing_File/Testing_File.ino
+++ Testing_File/Testing_File.ino
@@ -1,23 +1,80 @@
+//int threshold = ;
+int driveSpeed = 255;
+int motorL = 5;
+int motorLspeed = 4;
+int motorR = 3;
+int motorRspeed = 2;
+int delayTime = 3000;
+int mode = 7;
+
+void setup() {
+  
+  pinMode (motorLspeed, OUTPUT);
+  pinMode (motorL, OUTPUT);
+  pinMode (motorRspeed, OUTPUT);
+  pinMode (motorR, OUTPUT);
+  pinMode(mode, OUTPUT);
+
+}
+void stopRobot() {
+  
+  digitalWrite(motorR, HIGH);
+  analogWrite(motorRspeed, 0);
+  digitalWrite(motorL, HIGH);
+  analogWrite(motorLspeed, 0);
+  
+}
 void drive() {
-  //turn right motor forward
+  
   digitalWrite(motorR, HIGH);
   analogWrite(motorRspeed, driveSpeed);
-  //turn left motor forward
   digitalWrite(motorL, HIGH);
   analogWrite(motorLspeed, driveSpeed);
+  delay(delayTime);
+  stopRobot();
 
 }
-void setup() {
-  //Right Motor Setup
-  pinMode(motorR, OUTPUT);
-  pinMode(motorRspeed, OUTPUT);
 
-  //Left Motor Setup
-  pinMode(motorL, OUTPUT);
-  pinMode(motorLspeed, OUTPUT);
+void turnLeft() {
+
+  digitalWrite(motorR, HIGH);
+  analogWrite(motorRspeed, driveSpeed);
+  digitalWrite(motorL, LOW);
+  analogWrite(motorLspeed, driveSpeed);
+  delay(delayTime);
+  stopRobot();
+  
 }
 
+void turnRight () {
+  
+  digitalWrite(motorR, LOW);
+  analogWrite(motorRspeed, driveSpeed);
+  digitalWrite(motorL, HIGH);
+  analogWrite(motorLspeed, driveSpeed);
+  delay(delayTime);
+  stopRobot();
+  
+}
 void loop() {
-  drive()
-  delay(1111)
+  
+  /*if(getUSF() == threshold) {
+    if(getUSF() == threshold) {
+      turnLeft();
+    }
+    else {
+      turnRight();
+    }
+  }
+  else if(getUSR != threshold) {
+    turnRight();
+  }
+  else {
+    drive();
+  }
+*/
+digitalWrite(mode, HIGH);
+turnLeft();
+turnRight();
+
 }
